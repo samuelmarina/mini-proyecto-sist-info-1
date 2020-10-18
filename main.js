eebfe25aba9632868e4b6f8adfc6d2bbaa75127e
samuelmarina <samuellmarina@gmail.com>
2020-10-18 16:49:42 -0400

    Include submit function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Slideshow
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -10,19 +11,13 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("slide-item");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  // dots[slideIndex-1].className += " active";
 }
 
 const skills = [
@@ -44,18 +39,32 @@ const skills = [
   }
 ]
 
+//Skills
 function addSkills(){
   const wrapper = document.getElementById("bars")
   var bars = "";
-  console.log(wrapper);
 
   skills.forEach(item => {
     bars += '<h2>' + item.name + '</h2><div class="skill-container"><div class="skills" style="background-color: #00587a; width: ' + item.level + ';">' + item.level + '</div></div>'
   })
 
   wrapper.innerHTML = bars;
-  console.log(bars);
 }
 
 addSkills();
+
+//Form submit
+function send() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if(!name || !email || !message) {
+    return alert("One of the fields is empty. Try again")
+  }
+
+  console.log("Name: " + name, "Email: " + email, "Message: " + message);
+  
+  alert("Message sent")
+}
 
