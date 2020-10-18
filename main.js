aef4d1c8ff5faa10496c04f6b9153c31692a468d
samuelmarina <samuellmarina@gmail.com>
2020-10-18 15:03:30 -0400

    Add function to display skills

--- main.js
+++ main.js
@@ -22,5 +22,40 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
+
+const skills = [
+  {
+    name: "React Native",
+    level: "80%"
+  },
+  {
+    name: "JavaScript",
+    level: "70%"
+  },
+  {
+    name: "Java",
+    level: "80%"
+  },
+  {
+    name: "Git",
+    level: "90%"
+  }
+]
+
+function addSkills(){
+  const wrapper = document.getElementById("bars")
+  var bars = "";
+  console.log(wrapper);
+
+  skills.forEach(item => {
+    bars += '<h2>' + item.name + '</h2><div class="skill-container"><div class="skills" style="background-color: #00587a; width: ' + item.level + ';">' + item.level + '</div></div>'
+  })
+
+  wrapper.innerHTML = bars;
+  console.log(bars);
+}
+
+addSkills();
+
